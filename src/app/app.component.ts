1bb93c4c3b95d043c0e6a1fbd861c2e54feed00a
davidmirza <davemirza9@gmail.com>
2023-11-28 17:20:12 +0700

    init 2 api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-
+  constructor(
+    private client_ : HttpClient
+  ){
+  }
+  protected jsonParseReviver: ((key: string, value: any) => any) | undefined = undefined;
   DataInput: string="";
   DataMessage:string="";
+  resultBackendphp:string="";
   title = 'test_frontend';
   resultHelper:any = [];
 
@@ -19,14 +25,42 @@ export class AppComponent implements OnInit{
     
     return (Number(this.DataInput))?true:false;
   }
+  getData(data:any, url:string){
+    this.client_.post(url,data).subscribe((resp)=>{
+      this.resultHelper = resp;
+      });
+  }
+  
   gProsesBc(dt:string){
-
+    var  isNumber;
+    var url = "http://localhost:8080/checknumber";
+    var inputhelper = { 'num':this.DataInput };
+     this.client_.post(url,inputhelper).subscribe((resp)=>{
+     isNumber = resp;
+     console.log(isNumber)
+     this.resultHelper = [];
+    if(isNumber===true){
+      this.DataMessage="";
+      if(dt=="sg"){
+       this.getData(inputhelper,"http://localhost:8080/getsegitiga");
+      }
+      else if(dt=="pr"){
+        this.getData(inputhelper,"http://localhost:8080/getprima");
+      }
+      else{
+        this.getData(inputhelper,"http://localhost:8080/getganjil");
+      }
+    }
+    else{
+      this.DataMessage="Bukan Number";
+    }
+  });
   }
   gProses(dt:string){
    var check= this.gCheckNumber();
    this.resultHelper = [];
    if(!check){
-    this.DataMessage="Bukan Number"
+    this.DataMessage="Bukan Number";
    }
    else{
     this.DataMessage="";
@@ -34,7 +68,7 @@ export class AppComponent implements OnInit{
     if(dt=="gj"){
       for(var c=0;c<=num;c++){
         var isGanjil = c % 2;
-          var tmp =(isGanjil==0)? c+" Bukan Bilangan Ganjil":c+" Bilangan Ganjil";
+          var tmp =(isGanjil==0)? c+" Bukan Bilangan Ganjil": c+" Bilangan Ganjil";
           this.resultHelper.push(tmp);
       }
     }
